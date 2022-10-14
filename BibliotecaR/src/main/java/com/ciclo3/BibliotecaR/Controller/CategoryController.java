c7d60e74afc4fe0703d11b9db0c7184d9fe3505f
cazomose <carol.moreno.mt@usa.edu.co>
2022-10-13 19:40:44 -0500

    api/

--- BibliotecaR/src/main/java/com/ciclo3/BibliotecaR/Controller/CategoryController.java
+++ BibliotecaR/src/main/java/com/ciclo3/BibliotecaR/Controller/CategoryController.java
@@ -4,13 +4,15 @@ import com.ciclo3.BibliotecaR.Model.Category;
 import com.ciclo3.BibliotecaR.Service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/Category")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.POST, RequestMethod.DELETE})
+
 
 public class CategoryController {
     @Autowired
--- BibliotecaR/src/main/java/com/ciclo3/BibliotecaR/Controller/ReservationController.java
+++ BibliotecaR/src/main/java/com/ciclo3/BibliotecaR/Controller/ReservationController.java
@@ -4,13 +4,15 @@ import com.ciclo3.BibliotecaR.Model.Reservation;
 import com.ciclo3.BibliotecaR.Service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/Reservation")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.POST, RequestMethod.DELETE})
+
 
 public class ReservationController {
     @Autowired
